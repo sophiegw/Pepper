4617a4d1443fbba6370e16ecc9f751de4f5e1f12
Nagha51 <clement.puissant@hotmail.fr>
2017-05-31 17:00:20 +0200

    3 Threads

--- Scripts_Python/run_synchro.py
+++ Scripts_Python/run_synchro.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
 import naoqi
 from naoqi import ALProxy
 import time
 from threading import Thread, Event
 
-msg = "CONNECT ERROR"
 port = 9559
-ip_1 = "192.168.8.100"
+ip_1 = "192.168.8.101"
 ip_2 = "192.168.8.105"
-ip_3 = "192.168.8.105"
+ip_3 = "192.168.8.112"
 ip_4 = "192.168.8.105"
 
 #print(behavior_1.getInstalledBehaviors())
@@ -23,6 +26,7 @@ class c_thr(Thread):
         self.event = ev
         self.name = 'A'
         self.port = port
+        self.connected = False
     #END INIT
     def run(self):
     #BEGIN RUN
@@ -30,43 +34,53 @@ class c_thr(Thread):
             self.behavior = ALProxy("ALBehaviorManager", self.ip, self.port)
             self.RobotPosture = ALProxy("ALRobotPosture", self.ip, self.port)
             self.speech = ALProxy("ALTextToSpeech", self.ip, self.port)
+            self.connected = True
             print("GOOD CONNECT\n")
         except:
+            self.connected = False
             print("BAD TRY\n")
-            self.name = msg
-            exit()
+            sys.exit()
         self.event.wait()
-        print("set %c %d\n" % (self.name, time.time()))
         '''inserer la choregraphie'''
         #self.RobotPosture.goToPosture("Crouch", 0.5)
         #self.RobotPosture.goToPosture("StandZero", 0.5)
         #self.RobotPosture.goToPosture("StandInit", 0.5)
         #self.behavior.runBehavior("elephant/behavior_1")
-        #self.speech.say("Bonjour, je suis un elephant !")
+        tosay = "Bonjour, je suis" + self.name
+        self.speech.say(tosay)
+    def stop(self):
+        sys.exit()
     #END RUN
 #END Class
 ev = Event()
 thrd_1 = c_thr(ev)
-#thrd_2 = c_thr(ev)
-#thrd_3 = c_thr(ev)
+thrd_2 = c_thr(ev)
+thrd_3 = c_thr(ev)
 #thrd_4 = c_thr(ev)
-#thrd_2.name = 'B'
-#thrd_3.name = 'C'
+thrd_1.name = "Superman"
+thrd_2.name = "GrineLanterne"
+thrd_3.name = "FlashGordone"
 #thrd_4.name = 'D'
 thrd_1.ip = ip_1
-#thrd_2.ip = ip_2
-#thrd_3.ip = ip_3
+thrd_2.ip = ip_2
+thrd_3.ip = ip_3
 #thrd_4.ip = ip_4
 thrd_1.start()
-#thrd_2.start()
-#thrd_3.start()
+thrd_2.start()
+thrd_3.start()
 #thrd_4.start()
 print("before set\n")
+print("1", thrd_1.connected)
+print("2", thrd_2.connected)
+print("3", thrd_3.connected)
+
 #time.sleep(3)
-if (thrd_1.name != msg):
+if (thrd_1.connected == True and thrd_2.connected == True and thrd_3.connected == True):
     ev.set()
     print("after set\n")
-thrd_1.join()
+
+
+#thrd_1.join()
 #thrd_2.join()
 #thrd_3.join()
 #thrd_4.join()
